69b457eb1f92488c1daf375ce3ceded11819fa97
onebigbear <2656200788@qq.com>
2018-09-29 14:11:27 +0800

    POST请求执行，执行重定向，在模板中展示所有items

--- lists/tests.py
+++ lists/tests.py
@@ -16,13 +16,26 @@ class HomePageTest(TestCase):
 		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')
-		self.assertIn('A new list item', response.content.decode())
-		self.assertTemplateUsed(response, 'lists/home.html')
+		
+	
+	def test_redirects_after_POST(self):
+		response = self.client.post('/lists/',data={'item_text': 'A new list item'})
+		self.assertEqual(response.status_code,302)
+		self.assertEqual(response['location'],'/lists/')
 	
 	def test_only_saves_items_when_necessary(self):
-		'''只保存非空待办事项'''
+		'''只有在必要时候保存待办事项'''
 		self.client.get('/lists/')
 		self.assertEqual(Item.objects.count(), 0)
+	
+	def test_displays_all_list_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+		
+		response = self.client.get('/lists/')
+		
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2',response.content.decode())
 		
 
 class ItemModelTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 from .models import Item
 from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-	item = Item()
-	item.text = request.POST.get('item_text', '')
-	item.save()
-	return render(request, 'lists/home.html', {'new_item_text': item.text})
+	if request.method == 'POST':
+		new_item_text = request.POST['item_text']
+		Item.objects.create(text=new_item_text)
+		return redirect('/lists/')
+	items = Item.objects.all()
+	
+	return render(request, 'lists/home.html', {'items':items})
 
 
